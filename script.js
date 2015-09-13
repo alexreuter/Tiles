929778a9f40e3f55aad8c7b621e5727af0a0eb8c
Alex Reuter <alex_reuter@Alexs-MacBook-Air-2.local>
2015-09-13 16:55:02 -0500

    Random spazzing

---
+++ script.js
@@ -0,0 +1,71 @@
+$(function(){
+    
+/*******************************************************************************************************
+                                        CANVAS SETUP
+*******************************************************************************************************/
+    var c=document.getElementById("canvas");
+    var ctx=c.getContext("2d");
+/*******************************************************************************************************
+                                        CANVAS RESIZE
+*******************************************************************************************************/
+    canvas.width = window.innerWidth-(window.innerHeight*0.03);
+    canvas.height = window.innerHeight-(window.innerHeight*0.035);
+/*******************************************************************************************************
+                                    GAME VARIABLES
+*******************************************************************************************************/
+    var framerate = 30;
+
+/*******************************************************************************************************
+                                MAIN ANIMATION FUNCTION
+*******************************************************************************************************/
+    
+    
+    function main()
+    {
+        //CALL SPECIFIC ANIMATING FUNCTIONS HERE
+        
+        
+        ctx.fillStyle = getRandomColor();
+        ctx.fillRect(0,0,canvas.width,canvas.height);
+        drawTiles();
+    }
+    
+    setInterval(main,200);
+    //1000/framerate
+    
+    function drawTiles()
+    {
+        var tiles = 2;
+        var border = 10;
+        var width = 0;
+        var rows = 5;
+        
+        width = (canvas.width-(border*(tiles-1)))/tiles;
+        height = (canvas.height-(border*rows-1))/rows;
+        ctx.fillStyle = getRandomColor();
+        
+        for(x=0;x<rows;x++){
+            for (i=0;i<tiles;i++)
+            {    
+                ctx.fillRect(0+(width*i)+(border*i),0,width,height);
+                ctx.fillStyle = getRandomColor();
+                ctx.fillRect(0+(width*i)+(border*i),0+(height*(x+1)) + (border*(x+1)),width,height);
+            }
+        }        
+    }
+    
+    function getRandomColor() 
+    {
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++ ) 
+        {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+}
+    
+    
+    
+//END OF WINDOW READY FUNCTION.
+});
